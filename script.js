f17fc8dc42e7e8d6fa6bf7d1dfb784f32cb47c7a
unknown <aguilera.k02@gmail.com>
2023-01-26 21:17:47 -0500

    function to save text input into local storage

--- script.js
+++ script.js
@@ -7,7 +7,23 @@ currentDayEl.style.fontSize = "60px"
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+console.log(today.hour())
 $(function () {
+
+  var timeBlockParent = $(".container-fluid");
+  var textArea = $(".description")
+
+    //this = event.target = save button
+  timeBlockParent.on("click", ".saveBtn", function (event) {
+    
+    textArea = event.target.parentElement.children[1]
+    textAreaInput = textArea.value 
+    timeId = event.target.parentElement.id
+    localStorage.setItem(timeId, textAreaInput)
+  })
+  })
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -25,5 +41,3 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
-});
